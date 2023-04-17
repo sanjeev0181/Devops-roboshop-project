9e05bfe49c0892f5611a5560fac5b83c60af9ae3
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-17 15:38:15 +0530

    adding colour

--- p-devops/roboshop-shell/redis.sh
+++ p-devops/roboshop-shell/redis.sh
@@ -1,7 +1,20 @@
+# yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+# dnf module enable redis:remi-6.2 -y
+# yum install redis -y 
+# # edit the file and replace 127.0.0.0 to 0.0.0.0
+# #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+# systemctl enable redis 
+# systemctl start redis 
+echo -e "\e[36m>>>>>>>>> Install Redis Repos <<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>>>>> Install Redis <<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
-yum install redis -y 
-# edit the file and replace 127.0.0.0 to 0.0.0.0
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-systemctl enable redis 
-systemctl start redis 
+yum install redis -y
+
+echo -e "\e[36m>>>>>>>>> Update Redis Listen Address <<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+
+echo -e "\e[36m>>>>>>>>> Start Redis Service <<<<<<<<\e[0m"
+systemctl enable redis
+systemctl restart redis
