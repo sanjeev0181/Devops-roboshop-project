d518f1741a91d1b8303455630fa35af6d9df992e
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-19 22:08:47 +0530

    variable

---
+++ p-devops/learn-shell/03-variable.sh
@@ -0,0 +1,15 @@
+a=10
+name=Devops
+
+#print variable
+echo a= $a
+echo name = ${name}
+
+
+#
+#Date=2023-04-18
+Date= $(date +%F)
+echo Today day is ${Date}
+
+ARTH=$((2-3*4/2))
+echo ARTH = ${ARTH}
