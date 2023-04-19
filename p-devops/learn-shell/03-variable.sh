f08114069dbc94133f0d3b713a7096de7a7e501a
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-19 23:32:42 +0530

    Special Variables for input~

--- p-devops/learn-shell/03-variable.sh
+++ p-devops/learn-shell/03-variable.sh
@@ -13,3 +13,10 @@ echo Today day is ${Date}
 
 ARTH=$((2-3*4/2))
 echo ARTH = ${ARTH}
+
+#Special Variables for input
+echo script Name - $0
+echo first argument - $1
+echo second argument - $2
+echo all argument - $*
+echo no .of argument- $#
