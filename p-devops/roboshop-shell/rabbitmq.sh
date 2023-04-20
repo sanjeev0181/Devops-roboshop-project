2e901b33bf97f6684da77233334b483be39d9709
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-20 10:51:03 +0530

    chaning the rabbitmq_appuser_password

--- p-devops/roboshop-shell/rabbitmq.sh
+++ p-devops/roboshop-shell/rabbitmq.sh
@@ -1,12 +1,13 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")  
-source ${script_path}/common.sh# echo ${app_user}
-# echo $script_path
+source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 yum install erlang -y
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 yum install rabbitmq-server -y 
 systemctl enable rabbitmq-server 
 systemctl start rabbitmq-server 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop  ${rabbitmq_appuser_password}#roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
