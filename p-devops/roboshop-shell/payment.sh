7b10a470c95bc575f363a1ab5c12d956121df4e1
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-20 10:50:36 +0530

    chaning the rabbitmq_appuser_password

--- p-devops/roboshop-shell/payment.sh
+++ p-devops/roboshop-shell/payment.sh
@@ -2,6 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")  
 source ${script_path}/common.sh# echo ${app_user}
 # echo $script_path
+rabbitmq_appuser_password=$1
 
 echo -e "\e[36m>>>>>>>>> Install python <<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
@@ -25,6 +26,7 @@ echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>> Set systemD service <<<<<<<<\e[0m"
+sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" $script_path/payment.service 
 cp $script_path/payment.service    /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>>>>> start payment service <<<<<<<<\e[0m"
