6edf7de2fac9b7f405bbb82a545f305beddce328
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-20 12:01:42 +0530

    adding the print_head line in common.sh file

--- p-devops/roboshop-shell/common.sh
+++ p-devops/roboshop-shell/common.sh
@@ -1,35 +1,38 @@
 app_user =roboshop
 
+print_head() {
+  echo -e "\e[36m>>>>>>>>> $1 <<<<<<<<\e[0m"   
+}
 
 func_nodejs() {
-echo -e "\e[36m>>>>>>>>> Configuring Node js Repos <<<<<<<<\e[0m"
+print_head Configuring Node js Repos 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[36m>>>>>>>>> Install NodeJs  <<<<<<<<\e[0m"
+print_head Install NodeJs  
 yum install nodejs -y
 
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
+print_head Add Application User 
 useradd  ${app_user} #roboshop
 
-echo -e "\e[36m>>>>>>>>> Create Application Directory  <<<<<<<<\e[0m"
+print_head Create Application Directory  
 rm -rf /app
 mkdir /app 
 
-echo -e "\e[36m>>>>>>>>> Download App content  <<<<<<<<\e[0m"
+print_head Download App content  
 curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
 cd /app 
 
-echo -e "\e[36m>>>>>>>>> Unzip App content  <<<<<<<<\e[0m"
+print_head Unzip App content  
 unzip /tmp/${component}.zip
 #cd /app
 
-echo -e "\e[36m>>>>>>>>> Install Nodejs Dependencies  <<<<<<<<\e[0m"
+print_head Install Nodejs Dependencies  
 npm install 
 
-echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
+print_head Copy Catalogue SystemD file
 cp $script_path/${component}.service /etc/systemd/system/${component}.service
 
-echo -e "\e[36m>>>>>>>>> Start Cart Service  <<<<<<<<\e[0m"
+print_head Start Cart Service  
 systemctl daemon-reload
 systemctl enable ${component} 
 systemctl start ${component}
