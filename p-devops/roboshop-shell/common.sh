fbc583298b9742e2fffaf0232dd979ece2ececd4
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-20 11:37:28 +0530

    adding the function line

--- p-devops/roboshop-shell/common.sh
+++ p-devops/roboshop-shell/common.sh
@@ -1,1 +1,37 @@
 app_user =roboshop
+
+
+func_nodejs() {
+echo -e "\e[36m>>>>>>>>> Configuring Node js Repos <<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs  <<<<<<<<\e[0m"
+yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
+useradd  ${app_user} #roboshop
+
+echo -e "\e[36m>>>>>>>>> Create Application Directory  <<<<<<<<\e[0m"
+rm -rf /app
+mkdir /app 
+
+echo -e "\e[36m>>>>>>>>> Download App content  <<<<<<<<\e[0m"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+cd /app 
+
+echo -e "\e[36m>>>>>>>>> Unzip App content  <<<<<<<<\e[0m"
+unzip /tmp/${component}.zip
+#cd /app
+
+echo -e "\e[36m>>>>>>>>> Install Nodejs Dependencies  <<<<<<<<\e[0m"
+npm install 
+
+echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
+cp $script_path/${component}.service /etc/systemd/system/${component}.service
+
+echo -e "\e[36m>>>>>>>>> Start Cart Service  <<<<<<<<\e[0m"
+systemctl daemon-reload
+systemctl enable ${component} 
+systemctl start ${component}
+
+}
