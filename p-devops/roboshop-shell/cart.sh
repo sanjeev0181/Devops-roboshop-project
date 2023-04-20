de4d0c1a3c1f0643cf273077287536ba961a7f4b
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-20 11:38:58 +0530

    chaning the common line in common.sh

--- p-devops/roboshop-shell/cart.sh
+++ p-devops/roboshop-shell/cart.sh
@@ -1,37 +1,6 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")  
 source ${script_path}/common.sh
-# echo ${app_user}
-# echo $script_path
 
-echo -e "\e[36m>>>>>>>>> Configuring Node js Repos <<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+component=cart
 
-echo -e "\e[36m>>>>>>>>> Install NodeJs  <<<<<<<<\e[0m"
-yum install nodejs -y
-
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd  ${app_user} #roboshop
-
-echo -e "\e[36m>>>>>>>>> Create Application Directory  <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app 
-
-echo -e "\e[36m>>>>>>>>> Download App content  <<<<<<<<\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
-cd /app 
-
-echo -e "\e[36m>>>>>>>>> Unzip App content  <<<<<<<<\e[0m"
-unzip /tmp/cart.zip
-#cd /app
-
-echo -e "\e[36m>>>>>>>>> Install Nodejs Dependencies  <<<<<<<<\e[0m"
-npm install 
-
-echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
-cp $script_path/cart.service /etc/systemd/system/cart.service
-
-echo -e "\e[36m>>>>>>>>> Start Cart Service  <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable cart 
-systemctl start cart
--- p-devops/roboshop-shell/user.sh
+++ p-devops/roboshop-shell/user.sh
@@ -1,40 +1,10 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")  
 source ${script_path}/common.sh
-#exit
 
-echo -e "\e[36m>>>>>>>>> Configuring Node js Repos <<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+component=catalogue
 
-echo -e "\e[36m>>>>>>>>> Install NodeJs  <<<<<<<<\e[0m"
-yum install nodejs -y
-
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>>>>> Create Application Directory  <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app 
-
-echo -e "\e[36m>>>>>>>>> Download App content  <<<<<<<<\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
-cd /app
-
-echo -e "\e[36m>>>>>>>>> Unzip App content  <<<<<<<<\e[0m"
-unzip /tmp/user.zip
-#cd /app 
-
-echo -e "\e[36m>>>>>>>>> Install Nodejs Dependencies  <<<<<<<<\e[0m"
-npm install 
-
-echo -e "\e[36m>>>>>>>>> Set systemD service <<<<<<<<\e[0m"
-#cp user.service  /etc/systemd/system/user.service
-cp $script_path/user.service  /etc/systemd/system/user.service
-
-echo -e "\e[36m>>>>>>>>> start payment service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable user 
-systemctl start user
+func_nodejs
 
 echo -e "\e[36m>>>>>>>>> Set systemD service <<<<<<<<\e[0m"
 cp mongo.repo  /etc/yum.repos.d/mongo.repo
