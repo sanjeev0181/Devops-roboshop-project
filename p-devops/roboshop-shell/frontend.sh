fabb3151035ce4ab9bd54f32bac6f489908bb3cd
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-21 16:45:34 +0530

    frontent small changes

--- p-devops/roboshop-shell/frontend.sh
+++ p-devops/roboshop-shell/frontend.sh
@@ -3,27 +3,27 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 func_print_head "install nginx"
-apt install nginx -y  &>>$log_file
-func_status_check $?
+yum install nginx -y  &>>$log_file
+#func_status_check $?
 
 func_print_head  "Copy roboshop Config file"
 cp $script_path/roboshop.conf   /etc/nginx/default.d/roboshop.conf &>>$log_file
-func_status_check $?
+#func_status_check()  $?
 
 func_print_head "Clean Old App content"
 rm -rf /usr/share/nginx/html/*   &>>$log_file
-func_status_check $?
+#func_status_check $?
 
 func_print_head "Download App content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
-func_status_check $?
+#func_status_check $?
 
 func_print_head "Extracting App Content"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip  &>>$log_file
-func_status_check $?
+#func_status_check $?
 
 func_print_head "Start Nginx"
 systemctl restart nginx  &>>$log_file
 systemctl enable nginx   &>>$log_file
-func_status_check $?
+#func_status_check $?
