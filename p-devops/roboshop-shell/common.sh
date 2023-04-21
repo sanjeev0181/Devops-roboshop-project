cf55e1f727a391621f439f74d7a8ed3a72e53fcd
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-21 14:43:27 +0530

    adding logs and func_ptint in func_python

--- p-devops/roboshop-shell/common.sh
+++ p-devops/roboshop-shell/common.sh
@@ -152,17 +152,21 @@ func_java() {
 
 func_python() {
     func_print_head "Install python" 
-    yum install python36 gcc python3-devel -y  &>>log_file
-
+    yum install python36 gcc python3-devel -y  &>>$log_file
+    func_status_check $?
+    
     func_app_prereq
+    func_status_check $?
 
     func_print_head "Install Dependencies" 
-    pip3.6 install -r requirements.txt &>>log_file
+    pip3.6 install -r requirements.txt &>>$log_file
+    func_status_check $?
 
     func_print_head "Set systemD service" 
-    sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" $script_path/payment.service  &>>log_file
-
+    sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" $script_path/payment.service  &>>$log_file
     func_status_check $?
-    func_systemd_setup
 
+    func_print_head "system install status"
+    func_systemd_setup
+    func_status_check $?
 }
