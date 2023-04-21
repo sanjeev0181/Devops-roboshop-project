5c09d1ad018a6d222b6f5ac44341a5519ee0c8f2
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-21 08:52:41 +0530

    schema_setup and func_app_prereq

--- p-devops/roboshop-shell/common.sh
+++ p-devops/roboshop-shell/common.sh
@@ -1,54 +1,112 @@
 app_user =roboshop
 
-print_head() {
+func_print_head() {
   echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m"   
 }
 
-schema_setup() {
+#mongodb install
+func_schema_setup() {
     if [ "$schema_setup" == "mango"]; then
-      print_head Set systemD service 
+      func_print_head Set systemD service 
       cp $script_path/mongo.repo  /etc/yum.repos.d/mongo.repo
 
-      print_head Install mongodb client 
+      func_print_head Install mongodb client 
       yum install mongodb-org-shell -y
 
-      print_head Load schema 
+      func_print_head Load schema 
       mongo --hostmongodb-dev.rdevops72online </app/schema/${component}.js
+    if
+
+    if [ "$schema_setup" == "mysql" ]; then
+        func_print_head "Install mysql"
+        yum install mysql -y 
+
+        func_print_head "Load schema" 
+        mysql -h mysql-dev.rdevops72.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
     if
 }
 
+
+#common step on all line
+func_app_prereq() {
+  func_print_head "Add Application User"
+  useradd ${app_user}
+
+  func_print_head "Create Application Dir"
+  rm -rf /app
+  mkdir /app 
+
+  func_print_head "Download application content" 
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+
+  func_print_head "Extract Application content"
+  cd /app 
+  unzip /tmp/${component}.zip
+  #cd /app 
+
+  # func_print_head "Download mvn dependencies" 
+  # mvn clean package 
+  # mv target/${component}-1.0.jar ${component}.jar 
+}
+
+
+#systemd setup
+func_systemd_setup() {
+  func_print_head "Copy Catalogue SystemD file"
+  cp $script_path/${component}.service /etc/systemd/system/${component}.service
+
+  func_print_head "Start ${component} Service"
+  systemctl daemon-reload
+  systemctl enable ${component} 
+  systemctl start ${component}
+}
+
+
+
+#install Node js
 func_nodejs() {
-print_head "Configuring Node js Repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  func_print_head "Configuring Node js Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-print_head "Install NodeJs"
-yum install nodejs -y
+  func_print_head "Install NodeJs"
+  yum install nodejs -y
 
-print_head "Add Application User" 
-useradd  ${app_user} #roboshop
+  #func_app_prereq
+  func_print_head "Add Application User" 
+  useradd  ${app_user} #roboshop
 
-print_head "Create Application Directory"
-rm -rf /app
-mkdir /app 
+  func_print_head "Create Application Directory"
+  rm -rf /app
+  mkdir /app 
 
-print_head "Download App content"  
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
-cd /app 
+  func_print_head "Download App content"  
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+  cd /app 
 
-print_head "Unzip App content"  
-unzip /tmp/${component}.zip
-#cd /app
+  func_print_head "Unzip App content"  
+  unzip /tmp/${component}.zip
+  #cd /app
+  
+  func_print_head "Install Nodejs Dependencies"
+  npm install 
+  
+  func_schema_setup
+  func_systemd_setup
+  
 
-print_head "Install Nodejs Dependencies"
-npm install 
+}
 
-print_head "Copy Catalogue SystemD file"
-cp $script_path/${component}.service /etc/systemd/system/${component}.service
+#install 
+func_java() {
+  func_print_head "Install mvn" 
+  yum install maven -y
 
-print_head "Start Cart Service"
-systemctl daemon-reload
-systemctl enable ${component} 
-systemctl start ${component}
-schema_setup
+  func_app_prereq
+
+  func_print_head "Download mvn dependencies" 
+  mvn clean package 
+  mv target/${component}-1.0.jar ${component}.jar
+  func_schema_setup
+  func_systemd_setup
 
 }
--- p-devops/roboshop-shell/shipping.sh
+++ p-devops/roboshop-shell/shipping.sh
@@ -8,46 +8,6 @@ if [-z "$mysql_root_password"]; then
     exit
 if
 
-echo -e "\e[36m>>>>>>>>> Install mvn <<<<<<<<\e[0m"
-yum install maven -y
-
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>>>>> Create App Dir <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app 
-
-echo -e "\e[36m>>>>>>>>> Download app content <<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
-
-echo -e "\e[36m>>>>>>>>> Extract Add content <<<<<<<<\e[0m"
-cd /app 
-unzip /tmp/shipping.zip
-#cd /app 
-
-echo -e "\e[36m>>>>>>>>> Download mvn dependencies <<<<<<<<\e[0m"
-mvn clean package 
-mv target/shipping-1.0.jar shipping.jar 
-
-# echo -e "\e[36m>>>>>>>>> Set systemD service <<<<<<<<\e[0m"
-# cp shipping.service  /etc/systemd/system/shipping.service
-
-# echo -e "\e[36m>>>>>>>>> start payment service <<<<<<<<\e[0m"
-# systemctl daemon-reload
-# systemctl enable shipping 
-#systemctl start shipping
-
-echo -e "\e[36m>>>>>>>>> Install mysql <<<<<<<<\e[0m"
-yum install mysql -y 
-
-echo -e "\e[36m>>>>>>>>> Load schema <<<<<<<<\e[0m"
-mysql -h mysql-dev.rdevops72.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
-
-echo -e "\e[36m>>>>>>>>> Set systemD service <<<<<<<<\e[0m"
-cp $script_path/shipping.service  /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>>>> start payment service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping 
-systemctl restart shipping
+component="shipping"
+func_schema_setup=mysql
+func_java() 
