a624c8d11f1084ebd66bf659eb4afac4818ce91c
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-21 16:41:13 +0530

    frontent cha nge

--- p-devops/roboshop-shell/frontend.sh
+++ p-devops/roboshop-shell/frontend.sh
@@ -1,7 +1,6 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")  
-source ${script_path}/common.sh# echo ${app_user}
-# echo $script_path
+source ${script_path}/common.sh
 
 func_print_head "install nginx"
 apt install nginx -y  &>>$log_file
