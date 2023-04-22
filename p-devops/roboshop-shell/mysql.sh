8693cafee3511dcb6880e433e8a95b9024c18f22
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-22 19:01:44 +0530

    line 6 space in mysql.sh

--- p-devops/roboshop-shell/mysql.sh
+++ p-devops/roboshop-shell/mysql.sh
@@ -3,7 +3,7 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-if [-z "$mysql_root_password"]; then
+if [ -z "$mysql_root_password"]; then
     echo Input mysql root password Missing
     exit
 fi
