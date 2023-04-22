de8073ce921d7ce1bfcafe876ad731566ac8f28e
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-22 19:46:03 +0530

    func_stat_check 0

--- p-devops/roboshop-shell/rabbitmq.sh
+++ p-devops/roboshop-shell/rabbitmq.sh
@@ -10,22 +10,22 @@ fi
 
 func_print_head "Setup ErLang Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
-func_stat_check $?
+func_status_check $? 
 
 func_print_head "Setup RabbitMQ Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
-func_stat_check $?
+func_status_check $? 
 
 func_print_head "Install ErLang & RabbitMQ"
 yum install erlang rabbitmq-server -y &>>$log_file
-func_stat_check $?
+func_status_check $? 
 
 func_print_head "Start RabbitMQ Service"
 systemctl enable rabbitmq-server &>>$log_file
 systemctl restart rabbitmq-server &>>$log_file
-func_stat_check $?
+func_status_check $? 
 
 func_print_head "Add Application User in RabbtiMQ"
 rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_file
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
-func_stat_check $?
+func_status_check $? 
