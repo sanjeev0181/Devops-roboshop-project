c617a366306be78693f0e1356aa456fff3f5c419
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-25 10:39:01 +0530

    output file

---
+++ p-devops/learn-terraform/01-vars/output.tf
@@ -0,0 +1,11 @@
+# To print in shell script we use echo following is the syntax in shell
+# echo $sample_string
+output "sample_string" {
+  value = var.sample_string 
+}
+
+## In above case we are printing just variable, If variable is a combination of some string then following is the syntax.
+## ${} is mandatory if we include the variable inside a string.
+output "sample_string1" {
+  value = "Value of sample_string = ${var.sample_string}" 
+}
