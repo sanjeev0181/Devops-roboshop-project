46c2bd2007de1186a8c21de80a17c9cad6e60cfd
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-26 20:26:57 +0530

    adding the output files in main.tf

--- p-devops/learn-terraform/02-data-source/main.tf
+++ p-devops/learn-terraform/02-data-source/main.tf
@@ -1,9 +1,24 @@
 # https://registry.terraform.io/providers/hashicorp/aws/2.60.0/docs/data-sources/security_group
 
-data "aws_security_group" "selected" {
-    name="allows_tls"
+# data "aws_security_group" "selected" {
+#     name="allows_tls"
+# }
+
+# output "security_group_id" {
+#     value = data.aws_security_group.selected
+# }
+
+data "aws_security_groups" "test" {}
+
+data "aws_security_group" "single" {
+    count  = length(data.aws_security_groups.test.ids)
+    id = data.aws_security_group.test.ids[count.index]
 }
 
-output "security_group_id" {
-    value = data.aws_security_group.selected
+output "all_sg" {
+    value = data.aws_security_groups.test
+}
+
+output "single" {
+    value = data.aws_security_group.test
 }
