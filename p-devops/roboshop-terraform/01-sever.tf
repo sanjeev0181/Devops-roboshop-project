c81cadf366d69d2d622b9f487bd0fa7b73cfa618
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-26 12:58:31 +0530

    adding vpc_security_group_ids in 01-server.tf

--- p-devops/roboshop-terraform/01-sever.tf
+++ p-devops/roboshop-terraform/01-sever.tf
@@ -33,7 +33,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "mongodb"
   }
@@ -50,7 +50,7 @@ resource "aws_route53_record" "mongodb" {
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "catalogue"
   }
@@ -67,7 +67,7 @@ resource "aws_route53_record" "catalogue" {
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "redis"
   }
@@ -84,7 +84,7 @@ resource "aws_route53_record" "redis" {
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids =  [ data.aws_security_group.selected.id ]
   tags = {
     Name = "user"
   }
@@ -101,7 +101,7 @@ resource "aws_route53_record" "user" {
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "cart"
   }
@@ -118,7 +118,7 @@ resource "aws_route53_record" "cart" {
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "mysql"
   }
@@ -135,7 +135,7 @@ resource "aws_route53_record" "mysql" {
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "shipping"
   }
@@ -152,7 +152,7 @@ resource "aws_route53_record" "shipping" {
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "rabbitmq"
   }
@@ -169,7 +169,7 @@ resource "aws_route53_record" "rabbitmq" {
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "payment"
   }
