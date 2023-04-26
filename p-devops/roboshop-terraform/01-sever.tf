69b9391272223051473422feb3fc62480087a4b4
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-26 12:53:08 +0530

    chaning the owners id number in 01-server.tf

--- p-devops/roboshop-terraform/01-sever.tf
+++ p-devops/roboshop-terraform/01-sever.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "centos" {
-  owners           = ["973714476881"]
+  owners           = ["521045274894"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
