83e6847a2a6e8c71a25a8deb0497dd1a39a5c82b
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-26 20:48:28 +0530

    adding provisioner local-exec in main.tf

--- p-devops/learn-terraform/03-loops/count/main.tf
+++ p-devops/learn-terraform/03-loops/count/main.tf
@@ -1,3 +1,17 @@
 resource "null_resource" "null" {
     count = 20
 }
+
+resource "null_resource" "fruits" {
+   count = lenght(var.fruits)  
+
+   provisioner "local-exec" { 
+    command = "echo Fruit Name - $(var.fruits[count.index])"
+     
+   }
+}
+
+variable "fruits" {
+    default  = ["apply","banana","organa"]  
+    description = "(optional) describe your variable"
+}
