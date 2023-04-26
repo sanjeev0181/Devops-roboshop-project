e78149ac779e63434b25adac4b822af785bf7d44
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-04-26 20:30:36 +0530

    uncommit to commit 3 to 9 line in main.tf

--- p-devops/learn-terraform/02-data-source/main.tf
+++ p-devops/learn-terraform/02-data-source/main.tf
@@ -8,17 +8,17 @@
 #     value = data.aws_security_group.selected
 # }
 
-data "aws_security_groups" "test" {}
+# data "aws_security_groups" "test" {}
 
-data "aws_security_group" "single" {
-    count  = length(data.aws_security_groups.test.ids)
-    id = data.aws_security_group.test.ids[count.index]
-}
+# data "aws_security_group" "single" {
+#     count  = length(data.aws_security_groups.test.ids)
+#     id = data.aws_security_group.test.ids[count.index]
+# }
 
-output "all_sg" {
-    value = data.aws_security_groups.test
-}
+# output "all_sg" {
+#     value = data.aws_security_groups.test
+# }
 
-output "single" {
-    value = data.aws_security_group.test
-}
+# output "single" {
+#     value = data.aws_security_group.test
+# }
