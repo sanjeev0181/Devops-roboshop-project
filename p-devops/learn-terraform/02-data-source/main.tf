c0cf227fb93d7b7c86efd7e38698a755420cb707
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-02 11:15:22 +0530

    adding

--- p-devops/learn-terraform/02-data-source/main.tf
+++ p-devops/learn-terraform/02-data-source/main.tf
@@ -1,12 +1,12 @@
 # https://registry.terraform.io/providers/hashicorp/aws/2.60.0/docs/data-sources/security_group
 
-# data "aws_security_group" "selected" {
-#     name="allows_tls"
-# }
+data "aws_security_group" "selected" {
+    name="allows_tls"
+}
 
-# output "security_group_id" {
-#     value = data.aws_security_group.selected
-# }
+output "security_group_id" {
+    value = data.aws_security_group.selected
+}
 
 # data "aws_security_groups" "test" {}
 
--- p-devops/learn-terraform/README.md
+++ p-devops/learn-terraform/README.md
@@ -11,3 +11,11 @@
 9.Loops
 10.Conditions
 11.output
+
+
+### Terraform variable precedence
+
+1. -var-file
+2. *.auto.tfvars
+3. terraform.tfvars
+4. ask in cli to provide input
