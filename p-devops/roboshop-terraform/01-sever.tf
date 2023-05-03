ac616fe00f95b600f9505e2c0bd53f04b08a0cce
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-03 18:40:31 +0530

    some chaning in servier

--- p-devops/roboshop-terraform/01-sever.tf
+++ p-devops/roboshop-terraform/01-sever.tf
@@ -1,8 +1,8 @@
-data "aws_ami" "centos" {
-  owners           = ["521045274894"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-}
+# data "aws_ami" "centos" {
+#   owners           = ["521045274894"]
+#   most_recent      = true
+#   name_regex       = "Centos-8-DevOps-Practice"
+# }
 
 data "aws_security_group" "selected" {
     name="allows_tls"
