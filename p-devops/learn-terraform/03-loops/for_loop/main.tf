4f26c552f0f7e9d56ca90fc2e503c4d914a60298
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-03 13:12:24 +0530

    some chaning line in for_loop/main.tf

--- p-devops/learn-terraform/03-loops/for_loop/main.tf
+++ p-devops/learn-terraform/03-loops/for_loop/main.tf
@@ -1,10 +1,12 @@
 
 resource "null_resource" "fruits" {
-   #count = lenght(var.fruits)  
+   #count = length(var.fruits)  
+   for_each = var.fruits
 
    provisioner "local-exec" { 
     #command = "echo Fruit Name - $(var.fruits[count.index])"
-    command = "echo ${length(var.fruits)}"
+    #command = "echo ${length(var.fruits)}"
+    command = "echo Fruit Name - ${var.fruits[each.key]} - ${var.fruits[each.value]}"
      
    }
 }
