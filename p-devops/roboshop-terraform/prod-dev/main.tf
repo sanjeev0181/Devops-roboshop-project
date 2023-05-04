f29222e3c8e14873ef6eadb176ed1ac031b99d69
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-04 19:36:37 +0530

    adding

---
+++ p-devops/roboshop-terraform/prod-dev/main.tf
@@ -0,0 +1,47 @@
+components = {
+frontend = {
+      name = "frontend"
+      instance_type = "t2.micro"
+    }
+    mangodb = {
+      name = "mangodb"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t2.micro"
+    }
+    redis = {
+      name = "redis"
+      instance_type = "t2.micro"
+    }
+    user = {
+      name = "user"
+      instance_type = "t2.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t2.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t2.micro"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t2.micro"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t2.medium"
+    }
+    payment = {
+      name = "payment"
+      instance_type = "t2.micro"
+    }
+    dispatch = {
+      name = "dispatch"
+      instance_type = "t2.micro"
+    }
+  }
+}
